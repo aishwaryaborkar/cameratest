b17b298a38f934990691ca6997b773abc87ffe95
aditya7395 <aditya7395@gmail.com>
2017-04-25 19:32:37 -0700

    Storing the image in the gallery

--- app/src/main/java/com/example/aishwarya/cameratest/MainActivity.java
+++ app/src/main/java/com/example/aishwarya/cameratest/MainActivity.java
@@ -36,23 +36,15 @@ public class MainActivity extends AppCompatActivity {
         uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                //Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                //dispatchTakePictureIntent();
-                //startActivityForResult(intent, REQUEST_TAKE_PHOTO);
                 onLaunchCamera(view);
             }
         });
     }
 
     public void onLaunchCamera(View view) {
-        // create Intent to take a picture and return control to the calling application
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        //intent.putExtra(MediaStore.EXTRA_OUTPUT, getPhotoFileUri(photoFileName)); // set the image file name
 
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
         if (intent.resolveActivity(getPackageManager()) != null) {
-            // Start the image capture intent to take photo
             startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
         }
     }
@@ -63,16 +55,12 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 Bundle extras = data.getExtras();
                 Bitmap imageBitmap = (Bitmap) extras.get("data");
-//                mImageLabel.setImageBitmap(imageBitmap);
-//                encodeBitmapAndSaveToFirebase(imageBitmap);
                 Uri takenPhotoUri = getPhotoFileUri(photoFileName);
-                // by this point we have the camera photo on disk
                 Bitmap takenImage = BitmapFactory.decodeFile(takenPhotoUri.getPath());
-                // RESIZE BITMAP, see section below
-                // Load the taken image into a preview
-                ImageView ivPreview = (ImageView) findViewById(R.id.imageView);
-                ivPreview.setImageBitmap(imageBitmap);
-            } else { // Result was a failure
+                imageView = (ImageView) findViewById(R.id.imageView);
+                imageView.setImageBitmap(imageBitmap);
+                saveImageToGallery();
+            } else {
                 Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
             }
         }
@@ -80,33 +68,22 @@ public class MainActivity extends AppCompatActivity {
 
     // Returns the Uri for a photo stored on disk given the fileName
     public Uri getPhotoFileUri(String fileName) {
-        // Only continue if the SD Card is mounted
-        if (isExternalStorageAvailable()) {
-            // Get safe storage directory for photos
-            // Use `getExternalFilesDir` on Context to access package-specific directories.
-            // This way, we don't need to request external read/write runtime permissions.
             File mediaStorageDir = new File(
                     getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
 
-            // Create the storage directory if it does not exist
             if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
                 Log.d(APP_TAG, "failed to create directory");
             }
 
-            // Return the file target for the photo based on filename
             File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
 
-            // wrap File object into a content provider
-            // required for API >= 24
-            // See https://guides.codepath.com/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
             return FileProvider.getUriForFile(MainActivity.this, "com.example.aishwarya.cameratest.fileprovider", file);
-        }
-        return null;
     }
 
-    // Returns true if external storage for photos is available
-    private boolean isExternalStorageAvailable() {
-        String state = Environment.getExternalStorageState();
-        return state.equals(Environment.MEDIA_MOUNTED);
+    private void saveImageToGallery(){
+        imageView.setDrawingCacheEnabled(true);
+        Bitmap b = imageView.getDrawingCache();
+        MediaStore.Images.Media.insertImage(this.getContentResolver(), b,"test", "test");
     }
+
 }
